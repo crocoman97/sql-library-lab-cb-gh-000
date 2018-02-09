6f4fe81775a46ef1482e2518d1038dd71797f12b
Rauof M <abdelrauof197@gmail.com>
2018-02-09 05:11:01 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Series (id INTEGER PRIMARY KEY, title TEXT, author_id 
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id 
 INTEGER, subgenre_id INTEGER);
 
 CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
