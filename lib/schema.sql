9fea5bc622c35181b23de345fcfb8752a279a6d6
Rauof M <abdelrauof197@gmail.com>
2018-02-09 04:01:42 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -6,4 +6,4 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER
 , series_id INTEGER);
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto 
 TEXT, species TEXT,author_id INTEGER, series_id INTEGER);
-CREATE TABLE character_books ()
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, )
